5d26fabdfc3905350fa2856ec0c3866a8d91c713
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2014-05-27 23:54:56 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shefuniathletics::Application.config.secret_key_base = '75a511a87a957dec60bd53e4be9900bee6990553ec99685517db275e02b16efc792f4f832ad83cb3bbd61ffab0e978870c469eee3bc2ac803a17d03a2cb1c779'
