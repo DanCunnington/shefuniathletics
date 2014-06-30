7e5bc5917c70b2c3c4a87c0efa08d4a392032c39
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2014-06-30 20:26:08 +0100

    assets

---
+++ public/assets/main-3eabe69f947bd4753ea238b826e394ff.js
@@ -0,0 +1,55 @@
+$(function() {
+	var errors = $(".alert.alert-danger");
+	
+	$.each(errors, function(index) {
+		var error = $(errors[index]);
+		var html = "<div class='alert alert-dismissable alert-danger'>"
+  					+ "<button type='button' class='close' data-dismiss='alert'>×</button>"
+  					+ error.text()
+  					+ "</div>";
+		error.removeClass("alert").removeClass("alert-danger");
+		error.html(html);
+	});
+	
+	var successes = $(".alert.alert-success");
+	
+	$.each(successes, function(index) {
+		var success = $(successes[index]);
+		var html = "<div class='alert alert-dismissable alert-success'>"
+  					+ "<button type='button' class='close' data-dismiss='alert'>×</button>"
+  					+ success.text()
+  					+ "</div>";
+		success.removeClass("alert").removeClass("alert-success");
+		success.html(html);
+	});
+	
+	var notices = $(".alert.alert-info");
+	
+	$.each(notices, function(index) {
+		var notice = $(notices[index]);
+		var html = "<div class='alert alert-dismissable alert-info'>"
+  					+ "<button type='button' class='close' data-dismiss='alert'>×</button>"
+  					+ notice.text()
+  					+ "</div>";
+		notice.removeClass("alert").removeClass("alert-info");
+		notice.html(html);
+	});
+	
+	
+	
+});
+
+function dateFieldDefaultFixture() {
+	var x = document.getElementById("fixture_date");
+	if (x.innerHTML == "") {
+		x.value = "Please Choose a Date";
+	}
+}
+
+function dateFieldDefaultSocial() {
+	var x = document.getElementById("social_date");
+	if (x.innerHTML == "") {
+		x.value = "Please Choose a Date";
+	}
+}
+;
