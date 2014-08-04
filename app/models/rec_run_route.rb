31a3ff422d3b18d9147e2efc9fb1be05cb432635
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2014-08-04 22:35:33 +0100

    rec run validation

--- app/models/rec_run_route.rb
+++ app/models/rec_run_route.rb
@@ -3,6 +3,6 @@ class RecRunRoute < ActiveRecord::Base
 	validates :distance, presence: true
 	validates :difficulty, presence: true
 	VALID_LINK_REGEX = /\Ahttps:\/\/mapsengine.google.com\/+[\w+\-.]/i
-	#validates :google_maps_link, presence: true, format: {with: VALID_LINK_REGEX}
+	validates :google_maps_link, presence: true, format: {with: VALID_LINK_REGEX}
 
 end
