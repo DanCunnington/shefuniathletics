ecd0a5947fcb2713df577ddad863fb977edeb112
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2014-09-18 15:14:53 +0100

    record validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,6 @@
 class Record < ActiveRecord::Base
-	validates :event, presence: true
-	validates :event_type, presence: true
+	#validates :event, presence: true
+	#validates :event_type, presence: true
 	validates :athlete, presence: true
 	validates :time, presence: true
 	validates :competition, presence: true
