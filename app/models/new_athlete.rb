1a54bea867a3f92a2d2816021a96fbdf6271343b
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2015-02-16 14:41:49 +0000

    email validation

--- app/models/new_athlete.rb
+++ app/models/new_athlete.rb
@@ -3,7 +3,7 @@ class NewAthlete < ActiveRecord::Base
 	attr_accessor :first_name
 	attr_accessor :surname
 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@sheffield.ac.uk/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@sheffield\.ac\.uk/i
 	validates :email, presence: true, length: { maximum: 300 }, format: {with: VALID_EMAIL_REGEX }, 
 		uniqueness: { case_sensitive: false }
 
