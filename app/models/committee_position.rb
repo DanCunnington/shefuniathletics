c3b75cd925f4da17f3feb71845d9db98b87042bf
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2015-05-28 15:46:31 +0100

    committee positions now sortable

--- app/models/committee_position.rb
+++ app/models/committee_position.rb
@@ -1,8 +1,7 @@
 class CommitteePosition < ActiveRecord::Base
 	validates :position, presence: true
 	validates :name, presence: true
-	#validates :image_url, presence: true
-	validates :order, presence: true
+	validates :image_url, presence: true
 	validates :description, presence: true
 	default_scope { order("priority ASC") }
 end
