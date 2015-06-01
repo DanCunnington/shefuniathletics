3eb7cec5e13052e939cc1f201ab3b65bb5e23a0f
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2015-06-01 22:10:50 +0100

    changeable bg image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include AdminSessionsHelper
+  before_action :set_background
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def set_background
+
+  	background_image = SiteImage.where(reference:"background_image")
+    if (background_image != [])
+      baseCloudinaryURL = 'http://res.cloudinary.com/shefuniathletics/image/upload/'
+      @backgroundImageURL = baseCloudinaryURL+SiteImage.find_by(reference: "background_image").image_url
+    else
+      @background_image =  ""
+    end
+
+  	
   end
 
   helper_method :current_user
